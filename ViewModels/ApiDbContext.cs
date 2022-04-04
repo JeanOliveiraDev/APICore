bf71942aedd54cdd6529f9daea91f24ed5b46834
Jean Oliveira <jeanoliveiraJeansilva@hotmail.com>
2022-04-04 19:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ ViewModels/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Api.ViewModels
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
